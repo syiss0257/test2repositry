e630649da61b2e21923bc087ffea89158450cbd3
syiss0257 <galcolover53@gmail.com>
2014-07-15 14:31:47 +0900

    初期

---
+++ PageView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PageView
+//
+//  Created by ohtake shingo on 2014/07/14.
+//  Copyright (c) 2014年 ohtake shingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PageView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PageView
+//
+//  Created by ohtake shingo on 2014/07/14.
+//  Copyright (c) 2014年 ohtake shingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
